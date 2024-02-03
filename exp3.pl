22461de02fdfa0f4d414ea76a5b1dfdfadc82db4
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-02-03 20:52:27 +0530

    Solved the TOH problem

--- exp3.pl
+++ exp3.pl
@@ -1,2 +1,4 @@
-/* creating extended family tree with both rules and facts */
+/* Aim: Creating extended family tree with both rules and facts */
+
+
 
---
+++ exp4.pl
@@ -0,0 +1,23 @@
+/*
+Aim: Solving the tower of hanoi problem using prolog
+move(N,Source,Destination,Intermediate).
+*/
+
+move(1,X,Y,_):- 
+write("Move top disk from "),
+write(X),
+write(" to "),
+write(Y),
+nl.
+move(N,X,Y,Z):-
+N>1,
+M is N-1,
+move(M,X,Z,Y),
+move(1,X,Y,_),
+move(M,Z,Y,X).
+
+
+/*
+Queries:
+move(3,source,destination,intermediate).
+*/

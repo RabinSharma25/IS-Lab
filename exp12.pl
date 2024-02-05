c8ff13ead8ec210b12202526d5bb29102ace2350
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-02-05 23:17:56 +0530

    solved fibonacci sequence

--- exp12.pl
+++ exp12.pl
@@ -1,12 +1,21 @@
 /*Aim: Write a prolog program to find the fibonacci series*/
 
-fib(0,0).
-fib(1,1).
+% Base cases for Fibonacci sequence
+fibonacci(0, 0).
+fibonacci(1, 1).
 
-fib(F,N):-
-    N> 1,
-    N1 is N-1,
-    N2 is N-2,
-    fib(F1,N1),
-    fib(F2,N2),
-    F is F1 + F2.
+% Recursive rule for Fibonacci sequence
+fibonacci(N, Result) :-
+    N > 1,
+    N1 is N - 1,
+    N2 is N - 2,
+    fibonacci(N1, Result1),
+    fibonacci(N2, Result2),
+    Result is Result1 + Result2.
+
+% Predicate to print Fibonacci sequence up to N
+print_fibonacci(N) :-
+    between(0, N, X),
+    fibonacci(X, Result),
+    write(Result), write(' '),
+    fail.
